bdbf95954275558fc40acabf4dafd0c152bfa866
Donatas Lisauskas <donliskas@gmail.com>
2020-10-15 20:09:58 +0300

    Modified.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { LinksComponent } from './links/links.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutMeComponent,
     LinksComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule
