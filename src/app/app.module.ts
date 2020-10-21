6e126284cbb515b0d88cbc53b03706d7351ea6d5
Donatas Lisauskas <donliskas@gmail.com>
2020-10-21 12:46:10 +0300

    Added and modified bootstrap styles. The router is added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LinksComponent } from './links/links.component';
 import { SkillsContentComponent } from './skills-content/skills-content.component';
 import { InformationComponent } from './information/information.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,13 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     NavBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {path: 'aboutMe', component: AboutMeComponent},
+        {path: 'informationComponent', component: InformationComponent },
+        {path: '**', redirectTo: 'aboutMe', pathMatch: 'full'},
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
