79cd14126761e6d3d641bb09aebb31652d100779
Donatas Lisauskas <donliskas@gmail.com>
2020-10-22 20:12:33 +0300

    Modified About me.

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -11,17 +11,7 @@ export class AboutMeComponent implements OnInit {
   lastName = 'LISAUSKAS';
   title = 'ENTRY LEVEL (JUNIOR) FULL-STACK DEVELOPER OF JAVA';
 
-  aboutMe = 'I am a constantly learning person who keeps seeking opportunities to improve the\n' +
-    '        skills of an IT specialist.\n' +
-    '        During the course of the SDA academy, I have developed good skills and knowledge about Java, OOP, TDD, Clean\n' +
-    '        Code, MySQL, from the side of frameworks: Hibernate ORM, JDBC. Currently, I have initiated to learn Front-End\n' +
-    '        technologies: HTTP, HTML, CSS, JavaScript, and what I will learn in the future at the academy of SDA is Spring\n' +
-    '        Framework, project management by Agile, Scrum.\n' +
-    '        My main best qualities are thoroughness, creativity, and goal-orientation. While working on the sales manager\n' +
-    '        position I have gained strong skills like communication and negotiation, detail orientation, analytical\n' +
-    '        thinking, and sales.\n' +
-    '        I am looking for an internship place or junior position, where I can use my IT knowledge and skills. If you give\n' +
-    '        me a chance to grow in your company, you will see how fast I can learn and meet your company goals.';
+  aboutMe = 'I am a constantly learning person who keeps seeking opportunities to improve the skills of an IT specialist. I always take the responsibility for the given tasks and, as a team player, I motivate colleges to do the same. In problem situations, I try to stay objective to solve them in a professional matter. My main best qualities are thoroughness, creativity, and goal-orientation. While working on the sales manager position I have gained strong skills like communication and negotiation, detail orientation, analytical thinking, and sales. About my life, I have always felt that I must be doing something special like creation in my life. Only then I am feeling much better. When I discovered programming I understood that I can realize myself 100% therein. Therefore I decided to change my life to IT. I live under a slogan - A life is a creation. Dive into it. Currently, I am a student of the Java course, where I learn both back-end and front-end development. I am looking for a practice place or junior position, where I can use my IT knowledge and skills. If you give me a chance to grow in your company, you will see how fast I can learn and meet your company goals.';
 
   constructor() { }
 
