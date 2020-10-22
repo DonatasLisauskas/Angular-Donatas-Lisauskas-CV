dfb46bff73f70da7e57937f3f3b88c40311ee699
Donatas Lisauskas <donliskas@gmail.com>
2020-10-22 18:03:14 +0300

    the information component was split into a few new education and workExp components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsContentComponent } from './skills-content/skills-content.component';
-import { InformationComponent } from './information/information.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import {RouterModule} from '@angular/router';
 import { ContactsComponent } from './contacts/contacts.component';
+import { EducationComponent } from './education/education.component';
+import { WorkExpComponent } from './work-exp/work-exp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutMeComponent,
     SkillsContentComponent,
-    InformationComponent,
     NavBarComponent,
     ContactsComponent,
+    EducationComponent,
+    WorkExpComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
       [
         {path: 'about-me', component: AboutMeComponent},
-        {path: 'information-component', component: InformationComponent },
+        {path: 'education', component: EducationComponent },
+        {path: 'workExp', component: WorkExpComponent},
         {path: 'tech-skills', component: SkillsContentComponent},
         {path: 'contacts', component: ContactsComponent},
         {path: '**', redirectTo: 'about-me', pathMatch: 'full'},
