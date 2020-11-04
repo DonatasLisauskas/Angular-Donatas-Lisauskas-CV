cb8864df6e38b9603c30cba0d99f856d2471380e
Donatas Lisauskas <donliskas@gmail.com>
2020-11-04 13:54:50 +0200

    Modified.

--- src/app/skills-content/skills-content.component.ts
+++ src/app/skills-content/skills-content.component.ts
@@ -13,8 +13,8 @@ export class SkillsContentComponent implements OnInit {
     databases: ['MySQL, HSQLDB'],
     frameworks: ['JDBC', 'Hibernate ORM', 'Angular', 'JavaFX', 'JUnit / JUnit5 / Jupiter'],
     otherSkills: ['OPP', 'TDD', 'Clean Code and good practices'],
-    tools: ['IntelliJ IDEA', 'MySQL Workbench', 'Visual Studio Code', 'Postman', 'GIT', 'Node.js'],
-    inFuture: ['Front - end technologies', 'Spring Framework', 'Agile & Scrum of project management']
+    /*tools: ['IntelliJ IDEA', 'MySQL Workbench', 'Visual Studio Code', 'Postman', 'GIT', 'Node.js'],*/
+    inFuture: [/*'Front - end technologies', */'Spring Framework', 'Agile & Scrum of project management']
   };
 
   personalSkills = ['Thoroughness', 'Creativity Skills', 'Goal-orientation', 'Analytical thinking',
